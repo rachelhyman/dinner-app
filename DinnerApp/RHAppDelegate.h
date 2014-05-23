30be6c88892bbca000cb47de21e589b81bb48f28
Rachel Hyman <rachel@detroitlabs.com>
2014-05-23 10:53:56 -0400

    Initial setup.

---
+++ DinnerApp/RHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RHAppDelegate.h
+//  DinnerApp
+//
+//  Created by Rachel Hyman on 5/23/14.
+//  Copyright (c) 2014 Rachel Hyman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, "7.0"
+
+target "DinnerAppTests" do
+  pod 'Kiwi/XCTest', '~> 2.2'
+end
